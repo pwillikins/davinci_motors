cad74223e71ddf7b440bcaf55c6e74935b332bf4
Paul Stone <pmstone247@gmail.com>
2013-08-14 20:11:05 -0600

    Davinci_motors changes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DavinciMotors::Application.config.secret_key_base = 'ee544f8c2afb85478f5ff1c39dd389311626f3fb3928661edd7689bc0fba9f5b22ac98019a723723733331aeff5781f6b4b5d5fe6eca0a60f0e8c147e575c428'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DavinciMotors::Application.config.session_store :cookie_store, key: '_davinci_motors_session'
